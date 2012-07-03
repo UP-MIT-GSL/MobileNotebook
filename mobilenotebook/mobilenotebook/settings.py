0f91b971be03195f4570db1906a5a19c82976509
unknown <ELIZ@ELIZ-PC.(none)>
2012-07-04 06:40:03 +0800

    Created info.views.about and about.html to display idea synopsis in <home>/about

--- mobilenotebook/mobilenotebook/settings.py
+++ mobilenotebook/mobilenotebook/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/home/cassie/friendbook/friendbook.sqlite3.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,6 +106,7 @@ ROOT_URLCONF = 'mobilenotebook.urls'
 WSGI_APPLICATION = 'mobilenotebook.wsgi.application'
 
 TEMPLATE_DIRS = (
+	'mobilenotebook/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -119,9 +120,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+    'info'
 )
 
 # A sample logging configuration. The only tangible logging
--- mobilenotebook/mobilenotebook/urls.py
+++ mobilenotebook/mobilenotebook/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mobilenotebook.views.home', name='home'),
     # url(r'^mobilenotebook/', include('mobilenotebook.foo.urls')),
-
+    
+    url(r'^about$', 'info.views.about'),
+    
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
