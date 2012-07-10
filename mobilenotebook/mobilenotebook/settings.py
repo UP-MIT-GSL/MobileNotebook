5033d5a3f7c5fdecc47bacf7244621eba21a4762
Cassey Real <cassey_real@yahoo.com>
2012-07-10 17:25:05 +0800

    modified the DATABASES section

--- mobilenotebook/mobilenotebook/settings.py
+++ mobilenotebook/mobilenotebook/settings.py
@@ -14,9 +14,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'REAL',                      # Or path to database file if using sqlite3.
-        'USER': 'REAL',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'REAL',                    		  # Or path to database file if using sqlite3.
+        'USER': 'REAL',           		           # Not used with sqlite3.
+        'PASSWORD': '',                 			# Not used with sqlite3.
         'HOST': '10.50.27.31',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
