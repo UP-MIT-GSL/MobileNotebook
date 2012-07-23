0ca1838501598063063ec2898ff5170159038209
Cassey Real <cassey_real@yahoo.com>
2012-07-23 22:30:23 +0800

    temporarily changed db to sqlite3 for testing. comments with ">>>>>" are the lines that are temporarily removed. included webaccess in the list of apps.

--- mobilenotebook/settings.py
+++ mobilenotebook/settings.py
@@ -1,4 +1,4 @@
-import dj_database_url
+#>>>>>import dj_database_url
 
 # Django settings for mobilenotebook project.
 
@@ -13,16 +13,16 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'REAL',                    		  # Or path to database file if using sqlite3.
-        'USER': 'REAL',           		           # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',#>>>>>django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mobilenotebook.db',                    		  # Or path to database file if using sqlite3.
+        'USER': '',           		           # Not used with sqlite3.
         'PASSWORD': '',                 			# Not used with sqlite3.
-        'HOST': '10.50.27.31',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+#>>>>>DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -127,7 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'info'
+    'webaccess'
 )
 
 # A sample logging configuration. The only tangible logging
