3dbba7b85aa838891fc2388eff5bb8253d5aae24
Cassey Real <cassey_real@yahoo.com>
2012-07-28 14:13:25 +0800

    configured CustomUser to inherit User Model. Working. Tested.

--- webaccess/admin.py
+++ webaccess/admin.py
@@ -1,20 +1,16 @@
 from django.contrib import admin
-from webaccess.models import User
+from webaccess.models import CustomUser
 from webaccess.models import School
 from webaccess.models import Professor
 from webaccess.models import Note
-from webaccess.models import Member
 from webaccess.models import CourseNumber
 from webaccess.models import Enroll
-from webaccess.models import Membership
 from webaccess.models import WriteNote
 
 admin.site.register(School)
 admin.site.register(Professor)
-admin.site.register(User)
+admin.site.register(CustomUser)
 admin.site.register(Note)
-admin.site.register(Member)
 admin.site.register(CourseNumber)
 admin.site.register(Enroll)
-admin.site.register(Membership)
 admin.site.register(WriteNote)
--- webaccess/models.py
+++ webaccess/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
 from django.contrib import admin
 
 class School(models.Model):
@@ -20,25 +20,21 @@ class CourseNumber(models.Model):
     def __unicode__(self):
         return self.name
 
-class User(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+class CustomUser(User):
     school = models.ForeignKey(School)
     coursenumbers = models.ManyToManyField(CourseNumber, through='Enroll')
+    objects = UserManager()
 
     def __unicode__(self):
-        return self.firstname + " " + self.lastname
+        return self.first_name + " " + self.last_name
 
 class Enroll(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(CustomUser)
     coursenumber = models.ForeignKey(CourseNumber)
     date_joined = models.DateField()
 
     def __unicode__(self):
-        return self.user.firstname + " is enrolled to " + self.coursenumber.name
+        return self.user.first_name + " is enrolled to " + self.coursenumber.name
 
 class Note(models.Model):
     title = models.CharField(max_length=50)
@@ -47,21 +43,9 @@ class Note(models.Model):
         return self.title
 
 class WriteNote(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(CustomUser)
     notes = models.ForeignKey(Note)
     date_written = models.DateField()
 
     def __unicode__(self):
         return self.user.firstname + " wrote " + self.notes.title
-
-class Membership(models.Model):
-    user = models.ForeignKey(User)
-    coursenumber = models.ForeignKey(CourseNumber)
-    date_joined = models.DateField()
-
-    def __unicode__(self):
-        return self.user.firstname + " is a member of " + self.group.name
-
-class Member(models.Model):
-    user = models.ForeignKey(User)
-    coursenumber = models.ForeignKey(CourseNumber)  
