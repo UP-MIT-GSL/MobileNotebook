26e0346dd39d52e513773f5d6f2b23c0a4d78f8c
Cassey Real <cassey_real@yahoo.com>
2012-07-28 16:42:43 +0800

    user login tested and working

--- webaccess/views.py
+++ webaccess/views.py
@@ -8,4 +8,19 @@ def about(request):
     return render_to_response('about.html')
 
 def login_user(request):
-	return render_to_response('login.html')
+    state = 'Please log in below...'
+    username = password = ''
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                request.session['member_id'] = user.id
+                return HttpResponse("You are now logged in.")
+            else:
+                state = 'Account is inactive.'
+        else:
+            state = "Your username and/or password were incorrect."
+    return render_to_response('login.html',RequestContext(request,{'state' : state, 'username' : username, 'password' : password}))
